590b132be190064bd8d7ff31f1c56448ece4e7e0
ranianajla <ranianajla@apps.ipb.ac.id>
2024-02-15 01:27:09 +0700

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
         <div className="flex justify-between p-4 font-mono bg-[#9B4444]">
 
             <div className="flex font-sans font-light gap-8 text-white">
-            <p className="font-extrabold">Yura's Pastry</p>
+            <p className="font-extrabold">Yura&apos;s Pastry</p>
             <p>Product</p>
             <p>About</p>
             <p>Gallery</p>
